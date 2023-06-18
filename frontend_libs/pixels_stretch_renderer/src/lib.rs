4af28eeda05fa86658d2cfbb930441e901fd3a64
dbalsom <dbalsom+github@gmail.com>
2023-06-18 15:02:41 -0400

    Add custom stretching renderer

---
+++ frontend_libs/pixels_stretch_renderer/src/lib.rs
@@ -0,0 +1,390 @@
+/*
+    MartyPC Emulator 
+    (C)2023 Daniel Balsom
+    https://github.com/dbalsom/marty
+
+    ---------------------------------------------------------------------------
+
+    pixels_stretch_renderer::lib.rs
+    Implement a stretching renderer for Pixels when we want to fill the entire 
+    window without maintaining square pixels.
+
+    This module adapted from the rust Pixels crate.
+    https://github.com/parasyte/pixels
+
+    ---------------------------------------------------------------------------
+
+    Copyright 2019 Jay Oster
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+/// A logical texture size for a window surface.
+#[derive(Debug)]
+pub struct SurfaceSize {
+    pub width: u32,
+    pub height: u32,
+}
+
+use ultraviolet::Mat4;
+use wgpu::{
+    TextureDescriptor,
+    util::DeviceExt
+};
+
+
+fn create_texture_view(pixels: &pixels::Pixels, width: u32, height: u32) -> wgpu::TextureView {
+    let device = pixels.device();
+    let texture_descriptor = TextureDescriptor {
+        label: None,
+        size: pixels::wgpu::Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: pixels.render_texture_format(),
+        view_formats: &[pixels.render_texture_format()],
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::RENDER_ATTACHMENT,
+    };
+
+    device
+        .create_texture(&texture_descriptor)
+        .create_view(&wgpu::TextureViewDescriptor::default())
+}
+
+fn create_bind_group(
+    device: &wgpu::Device,
+    bind_group_layout: &wgpu::BindGroupLayout,
+    texture_view: &wgpu::TextureView,
+    sampler: &wgpu::Sampler,
+    time_buffer: &wgpu::Buffer,
+) -> pixels::wgpu::BindGroup {
+    device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(texture_view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(sampler),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: time_buffer.as_entire_binding(),
+            },
+        ],
+    })
+}
+
+/// The default renderer that scales your frame to the screen size.
+#[derive(Debug)]
+pub struct StretchingRenderer {
+    texture_view: wgpu::TextureView,
+    sampler: wgpu::Sampler,
+    bind_group_layout: wgpu::BindGroupLayout,
+    bind_group: wgpu::BindGroup,
+    render_pipeline: wgpu::RenderPipeline,
+    uniform_buffer: wgpu::Buffer,
+    vertex_buffer: wgpu::Buffer,
+    texture_width: u32,
+    texture_height: u32,
+    screen_width: u32,
+    screen_height: u32,
+
+}
+
+impl StretchingRenderer {
+    pub fn new(
+        pixels: &pixels::Pixels,
+        texture_width: u32,
+        texture_height: u32,
+        screen_width: u32,
+        screen_height: u32,
+    ) -> Self {
+
+        let device = pixels.device();
+        let shader = wgpu::include_wgsl!("./shaders/scale.wgsl");
+        let module = device.create_shader_module(shader);
+
+        //let texture_view = create_texture_view(pixels, screen_width, screen_height);
+        let texture_view = pixels.texture().create_view(&wgpu::TextureViewDescriptor::default());
+
+        // Create a texture sampler with nearest neighbor
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label: Some("pixels_stretching_renderer_sampler"),
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            lod_min_clamp: 0.0,
+            lod_max_clamp: 1.0,
+            compare: None,
+            anisotropy_clamp: None,
+            border_color: None,
+        });
+
+        // Create vertex buffer; array-of-array of position and texture coordinates
+            // One full-screen triangle
+            // See: https://github.com/parasyte/pixels/issues/180        
+        let vertex_data: [[f32; 2]; 3] = [
+            [-1.0, -1.0],
+            [3.0, -1.0],
+            [-1.0, 3.0],
+        ];
+        let vertex_data_slice = bytemuck::cast_slice(&vertex_data);
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("pixels_stretching_renderer_vertex_buffer"),
+            contents: vertex_data_slice,
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+        let vertex_buffer_layout = wgpu::VertexBufferLayout {
+            array_stride: (vertex_data_slice.len() / vertex_data.len()) as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x2,
+                offset: 0,
+                shader_location: 0,
+            }],
+        };
+
+        // Create uniform buffer
+        let matrix = ScalingMatrix::new(
+            (texture_width as f32, texture_height as f32),
+            (screen_width as f32, screen_height as f32),
+        );
+        let transform_bytes = matrix.as_bytes();
+        let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("pixels_stretching_renderer_matrix_uniform_buffer"),
+            contents: transform_bytes,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        // Create bind group
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("pixels_stretching_renderer_bind_group_layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(transform_bytes.len() as u64),
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        let bind_group = create_bind_group(
+            device,
+            &bind_group_layout,
+            &texture_view,
+            &sampler,
+            &uniform_buffer,
+        );
+
+        // Create pipeline
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("pixels_stretching_renderer_pipeline_layout"),
+            bind_group_layouts: &[&bind_group_layout],
+            push_constant_ranges: &[],
+        });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("pixels_stretching_renderer_pipeline"),
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &module,
+                entry_point: "vs_main",
+                buffers: &[vertex_buffer_layout],
+            },
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            fragment: Some(wgpu::FragmentState {
+                module: &module,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: pixels.render_texture_format(),
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent::REPLACE,
+                        alpha: wgpu::BlendComponent::REPLACE,
+                    }),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+        });
+
+        Self {
+            texture_view,
+            sampler,
+            bind_group_layout,
+            bind_group,
+            render_pipeline,
+            uniform_buffer,
+            vertex_buffer,
+            texture_width,
+            texture_height,
+            screen_width,
+            screen_height
+        }
+    }
+
+    pub fn get_texture_view(&self) -> &wgpu::TextureView {
+        &self.texture_view
+    }
+
+    /// Draw the pixel buffer to the render target.
+    pub fn render(
+        &self,
+        encoder: &mut wgpu::CommandEncoder,
+        render_target: &wgpu::TextureView,
+    ) {
+        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("StretchRenderer render pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: render_target,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        });
+        rpass.set_pipeline(&self.render_pipeline);
+        rpass.set_bind_group(0, &self.bind_group, &[]);
+        rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+
+        /*
+        rpass.set_scissor_rect(
+            0,
+            0,
+            1,
+            1
+        );
+        */
+        
+        rpass.draw(0..3, 0..1);
+    }
+
+    pub fn resize(
+        &mut self,
+        pixels: &pixels::Pixels,
+        texture_width: u32,
+        texture_height: u32,
+        screen_width: u32,
+        screen_height: u32,
+    ) {
+        self.texture_view = create_texture_view(pixels, screen_width, screen_height);
+        self.bind_group = create_bind_group(
+            pixels.device(),
+            &self.bind_group_layout,
+            &self.texture_view,
+            &self.sampler,
+            &self.uniform_buffer,
+        );
+
+        let matrix = ScalingMatrix::new(
+            (texture_width as f32, texture_height as f32),
+            (screen_width as f32, screen_height as f32),
+        );
+        let transform_bytes = matrix.as_bytes();
+        pixels
+            .queue()
+            .write_buffer(&self.uniform_buffer, 0, transform_bytes);
+    }
+}
+
+#[derive(Debug)]
+
+struct ScalingMatrix {
+    transform: Mat4,
+}
+
+impl ScalingMatrix {
+    // texture_size is the dimensions of the drawing texture
+    // screen_size is the dimensions of the surface being drawn to
+    fn new(texture_size: (f32, f32), screen_size: (f32, f32)) -> Self {
+        let (texture_width, texture_height) = texture_size;
+        let (screen_width, screen_height) = screen_size;
+
+        // Get smallest scale size
+        let scale = (screen_width / texture_width)
+            .min(screen_height / texture_height)
+            .max(1.0);
+
+        let vert_scale = screen_height / texture_height;
+
+        let scaled_width = texture_width * scale;
+        //let scaled_height = texture_height * vert_scale;
+
+        // Create a transformation matrix
+        let sw = scaled_width / texture_width;
+        let sh = vert_scale;
+        //let tx = (texture_width / 2.0).fract() / texture_width;
+        //let ty = (screen_height / 2.0).fract() / screen_height;
+
+        let ty = -(screen_height - texture_height) / screen_height;
+        //log::warn!("using ty of: {}", ty);
+        let tx = 0.0;
+    
+        #[rustfmt::skip]
+        let transform: [f32; 16] = [
+            sw,  0.0, 0.0, 0.0,
+            0.0, sh,  0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, ty,  0.0, 1.0,
+        ];
+
+        Self {
+            transform: Mat4::from(transform),
+        }
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        self.transform.as_byte_slice()
+    }
+}
--- frontends/martypc_pixels_wasm32/src/lib.rs
+++ frontends/martypc_pixels_wasm32/src/lib.rs
@@ -3,6 +3,7 @@
 
 use instant::{Instant, Duration};
 
+use pixels::wgpu::TextureView;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
@@ -23,10 +24,12 @@ use error_iter::ErrorIter as _;
 use log::error;
 use pixels::{Pixels, SurfaceTexture};
 use std::rc::Rc;
-use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::{
+    dpi::{PhysicalSize, LogicalSize},
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder
+};
 use winit_input_helper::WinitInputHelper;
 
 use marty_core::{
@@ -51,16 +54,18 @@ use marty_core::{
 };
 
 use marty_render::{VideoData, VideoRenderer, CompositeParams, ResampleContext};
+use pixels_stretch_renderer::{StretchingRenderer, SurfaceSize};
 
-const DEFAULT_RENDER_WIDTH: u32 = 640;
-const DEFAULT_RENDER_HEIGHT: u32 = 400;
+const DEFAULT_RENDER_WIDTH: u32 = 768;
+const DEFAULT_RENDER_HEIGHT: u32 = 524;
+
+const DEFAULT_ASPECT_WIDTH: u32 = 768;
+const DEFAULT_ASPECT_HEIGHT: u32 = 576;
+
+
 const MIN_RENDER_WIDTH: u32 = 160;
 const MIN_RENDER_HEIGHT: u32 = 200;
 const RENDER_ASPECT: f32 = 0.75;
-
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
-const BOX_SIZE: i16 = 64;
 
 pub const FPS_TARGET: f64 = 60.0;
 const MICROS_PER_FRAME: f64 = 1.0 / FPS_TARGET * 1000000.0;
@@ -149,9 +154,14 @@ fn start() {
     #[cfg(target_arch = "wasm32")]
     {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console_log::init_with_level(log::Level::Warn).expect("Error initializing logger!");
+        
+        log::warn!("Initializing logger...");
+        match console_log::init_with_level(log::Level::Warn) {
+            Ok(()) => {},
+            Err(e) => log::error!("Couldn't initialize logger: {}", e)
+        };
 
-        wasm_bindgen_futures::spawn_local(run());
+        //wasm_bindgen_futures::spawn_local(run());
     }
 
     #[cfg(not(target_arch = "wasm32"))]
@@ -204,7 +214,8 @@ fn read_blob_as_array_buffer(blob: &web_sys::Blob) -> js_sys::Promise {
     promise
 }
 
-async fn run() {
+#[wasm_bindgen]
+pub async fn run() {
 
     // Emulator stuff
     let mut stat_counter = Counter::new();
@@ -212,8 +223,8 @@ async fn run() {
     let mut video_data = VideoData {
         render_w: DEFAULT_RENDER_WIDTH,
         render_h: DEFAULT_RENDER_HEIGHT,
-        aspect_w: 640,
-        aspect_h: 480,
+        aspect_w: DEFAULT_ASPECT_WIDTH,
+        aspect_h: DEFAULT_ASPECT_HEIGHT,
         aspect_correction_enabled: false,
         composite_params: Default::default(),
     };
@@ -230,9 +241,10 @@ async fn run() {
     // Winit stuff
     let event_loop = EventLoop::new();
     let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let size = LogicalSize::new(DEFAULT_ASPECT_WIDTH as f64, DEFAULT_ASPECT_HEIGHT as f64);
+
         WindowBuilder::new()
-            .with_title("Hello Pixels + Web")
+            .with_title("MartyPC WASM Player")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
@@ -242,6 +254,8 @@ async fn run() {
     let window = Rc::new(window);
 
     let mut machine;
+
+    
 
     #[cfg(target_arch = "wasm32")]
     {
@@ -260,25 +274,30 @@ async fn run() {
         let window = Rc::clone(&window);
 
         // Initialize winit window with current dimensions of browser client
-        window.set_inner_size(get_window_size());
+        //window.set_inner_size(get_window_size());
+        window.set_inner_size(PhysicalSize::new(768, 576));
 
         let client_window = web_sys::window().unwrap();
 
         // Attach winit canvas to body element
         web_sys::window()
             .and_then(|win| win.document())
-            .and_then(|doc| doc.body())
-            .and_then(|body| {
-                body.append_child(&web_sys::Element::from(window.canvas()))
+            .and_then(|doc| {
+                // Here we use query_selector to get the element by class name.
+                doc.query_selector("#marty-canvas-container").ok().flatten()
+            })
+            .and_then(|div| {
+                // Append the canvas to the div.
+                div.append_child(&web_sys::Element::from(window.canvas()))
                     .ok()
             })
-            .expect("Couldn't append canvas to document body!");
+            .expect("Couldn't append canvas to the specified div!");
 
         // Try to load toml config.
         let mut opts = web_sys::RequestInit::new();
         opts.method("GET");
 
-        let request = web_sys::Request::new_with_str_and_init("martypc_wasm.toml", &opts).expect("Couldn't create request for configuration file.");
+        let request = web_sys::Request::new_with_str_and_init("./cfg/martypc_wasm.toml", &opts).expect("Couldn't create request for configuration file.");
         request.headers().set("Content-Type", "text/plain").expect("Couldn't set headers!");
 
         let resp_value = JsFuture::from(client_window.fetch_with_request(&request)).await.unwrap();
@@ -395,6 +414,7 @@ async fn run() {
             }
         }
 
+        /*
         // Listen for resize event on browser client. Adjust winit window dimensions
         // on event trigger
         let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
@@ -407,6 +427,7 @@ async fn run() {
             .unwrap();
 
         closure.forget();
+        */
     }
 
     let mut input = WinitInputHelper::new();
@@ -414,11 +435,19 @@ async fn run() {
         let window_size = window.inner_size();
         let surface_texture =
             SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
-        Pixels::new_async(WIDTH, HEIGHT, surface_texture)
+
+        Pixels::new_async(DEFAULT_RENDER_WIDTH, DEFAULT_RENDER_HEIGHT, surface_texture)
             .await
             .expect("Pixels error")
     };
-    let mut world = World::new();
+
+    let stretching_renderer = StretchingRenderer::new(
+        &pixels,
+        video_data.render_w,
+        video_data.render_h,
+        video_data.aspect_w,
+        video_data.aspect_h,
+    );
 
     event_loop.run(move |event, _, control_flow| {
 
@@ -633,34 +662,51 @@ async fn run() {
             //world.draw(pixels.frame_mut());
 
             //stat_counter.current_fps += 1;
+            
+            if let Err(e) = pixels.render_with(
+                |encoder, render_target, context| {
+                    let fill_texture = stretching_renderer.get_texture_view();
+                    
+                    //context.scaling_renderer.render(encoder, fill_texture);
+    
+                    stretching_renderer.render(encoder, render_target);
+                    Ok(())
+                }
+            ) {
+                log::error!("pixels.render_with error: {}", e);
+            };
 
+            /*
             if let Err(err) = pixels.render() {
                 log_error("pixels.render", err);
                 *control_flow = ControlFlow::Exit;
                 return;
             }
+            */
         }
 
         // Handle input events
         if input.update(&event) {
             // Close events
             if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
+                
+                // No point in quitting if someone hits escape in the browser.
+                //*control_flow = ControlFlow::Exit;
                 return;
             }
 
             // Resize the window
             if let Some(size) = input.window_resized() {
+                /*
                 if let Err(err) = pixels.resize_surface(size.width, size.height) {
                     log_error("pixels.resize_surface", err);
                     *control_flow = ControlFlow::Exit;
                     return;
                 }
+                */
             }
 
-            // Update internal state and request a redraw
-            //world.update();
-
+            // Request a redraw
             window.request_redraw();
         }
     });
@@ -670,53 +716,5 @@ fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
     error!("{method_name}() failed: {err}");
     for source in err.sources().skip(1) {
         error!("  Caused by: {source}");
-    }
-}
-
-impl World {
-    /// Create a new `World` instance that can draw a moving box.
-    fn new() -> Self {
-        Self {
-            box_x: 24,
-            box_y: 16,
-            velocity_x: 1,
-            velocity_y: 1,
-        }
-    }
-
-    /// Update the `World` internal state; bounce the box around the screen.
-    fn update(&mut self) {
-        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
-            self.velocity_x *= -1;
-        }
-        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
-            self.velocity_y *= -1;
-        }
-
-        self.box_x += self.velocity_x;
-        self.box_y += self.velocity_y;
-    }
-
-    /// Draw the `World` state to the frame buffer.
-    ///
-    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
-    fn draw(&self, frame: &mut [u8]) {
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % WIDTH as usize) as i16;
-            let y = (i / WIDTH as usize) as i16;
-
-            let inside_the_box = x >= self.box_x
-                && x < self.box_x + BOX_SIZE
-                && y >= self.box_y
-                && y < self.box_y + BOX_SIZE;
-
-            let rgba = if inside_the_box {
-                [0x5e, 0x48, 0xe8, 0xff]
-            } else {
-                [0x48, 0xb2, 0xe8, 0xff]
-            };
-
-            pixel.copy_from_slice(&rgba);
-        }
     }
 }
