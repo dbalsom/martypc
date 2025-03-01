4fba01ab3be239125965b3af2d5bd5466f587bed
dbalsom <dbalsom+github@gmail.com>
2025-03-01 15:19:34 -0500

    remove 'unstable' section from ./cargo/config.toml. it is not target specific. add to env instead for each build script

--- crates/frontends/martypc_eframe/wasm-build.bat
+++
@@ -1,2 +0,0 @@
-set MARTYPC_URL_BASE=http://localhost:8080
-trunk build --release=true --no-default-features --features=all_video_cards,sound,use_glow --config Trunk.toml
--- crates/frontends/martypc_eframe/wasm-serve-glow.bat
+++ crates/frontends/martypc_eframe/wasm-serve-glow.bat
@@ -1,2 +1,3 @@
 set MARTYPC_URL_BASE=http://localhost:8080
-trunk serve --no-default-features --features=all_video_cards,sound,use_glow --config Trunk.toml
+set CARGO_UNSTABLE_BUILD_STD=std,panic_abort
+trunk serve --release=true --no-default-features --features=all_video_cards,sound,use_glow --config Trunk.toml
