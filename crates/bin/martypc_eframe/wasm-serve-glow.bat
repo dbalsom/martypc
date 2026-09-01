a244068f14c1962ae4097130ee70b576385b1552
dbalsom <dbalsom+github@gmail.com>
2026-09-01 13:14:49 -0400

    rename MARTYPC_URL_BASE to MARTYPC_BASE_URL

--- crates/bin/martypc_eframe/wasm-serve-glow.bat
+++ crates/bin/martypc_eframe/wasm-serve-glow.bat
@@ -1,3 +1,3 @@
-set MARTYPC_URL_BASE=http://localhost:8080
+set MARTYPC_BASE_URL=http://localhost:8000
 set CARGO_UNSTABLE_BUILD_STD=std,panic_abort
 trunk serve --release=true --no-default-features  --features=wasm_glow --config Trunk.toml
